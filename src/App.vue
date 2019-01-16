0b4b1384e11c48a3f52e2a8ce8fbaf016fdef9ad
Sleyter Sandoval <ssandoval@epcmexperts.com>
2019-01-16 15:24:14 -0500

    adding routing and shopping cart

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div id="app">
-    <store />
-  </div>
+  <router-view />
 </template>
 
 <script>
-import Store from './components/Store.vue'
+//import Store from './components/Store.vue'
+import { mapActions } from 'vuex';
 
 export default {
   name: 'app',
+  /*
   components: {
     Store
+  },
+  */
+  methods:{
+    ...mapActions(["getData"])
+  },
+  created(){
+    this.getData();
   }
 }
 </script>
